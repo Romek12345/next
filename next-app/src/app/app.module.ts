f0cc46383f37bc201329473dcbde347ad01042f9
Romek12345 <s-okrartes@wp.pl>
2021-07-05 20:53:58 +0200

    stworzenie głównych komponentów

--- next-app/src/app/app.module.ts
+++ next-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedComponent } from './shared/shared.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { First } from './body/first.box/first.box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    First.BoxComponent
   ],
   imports: [
     BrowserModule,
---
+++ next-app/src/app/body/first.box/first.box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first.box',
+  templateUrl: './first.box.component.html',
+  styleUrls: ['./first.box.component.scss']
+})
+export class First.BoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
