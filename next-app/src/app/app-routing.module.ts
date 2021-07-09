25edeb84672f1309a17683d9e9f7ce6eb88fdc41
Romek12345 <s-okrartes@wp.pl>
2021-07-09 16:43:25 +0200

    tylko footer

--- next-app/src/app/app-routing.module.ts
+++ next-app/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component'
+import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'body', component: BodyComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+ // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: FooterComponent },
+  // path** dziala tylko, i nie wczytuje reszty komponentow(body,headera)
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
